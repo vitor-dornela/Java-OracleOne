eae85fd2b94fd169c360da70142b90ba60b936b5
vitor-dornela <vitor.dornela.m@gmail.com>
2024-12-28 22:17:03 -0300

    1-2

--- src/TextBlock.java
+++ src/TextBlock.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class TextBlock {
+    public static void main(String[] args) {
+        String nome = "João";
+        int aulas = 4;
+
+        String mensagem = """
+                  Olá, %s!
+                  Boas vindas ao curso de Java.
+                  Teremos %d aulas para te mostrar o que é preciso para você dar o seu primeiro mergulho na linguagem!
+                  """.formatted(nome, aulas);
+
+        System.out.println(mensagem);
+    }
 }
